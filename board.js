01828846ed550437d7dc5f402f014cedde38d1d3
Alejandro Garcia <agarciacoding@gmail.com>
2022-12-17 19:08:23 -0600

    update

--- board.js
+++ board.js
@@ -7,4 +7,42 @@ class Board {
             {length: ROWS}, ()=> Array(COLS).fill(0)
         )
     }
+
+    valid(p) {
+        return p.shape.every((row,dy)=> {
+            return row.every((value,dx) => {
+                let x = p.x + dx;
+                let y = p.y + dy;
+                return (
+                    this.insideWalls(x) && this.aboveFloor(y)
+                );
+            });
+        });
+    };
+
+    insideWalls(x) {
+        return x >= 0 && x< COLS;
+    }
+
+    aboveFloor(y){
+        return y <= ROWS;
+    }
+
+    rotate(p){
+        // Clone with JSON for immutability
+        let clone = JSON.parse(JSON.stringify(p));
+        // Transpose matrix, p is the Piece.
+        for (let y = 0; y < p.shape.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [p.shape[x][y], p.shape[y][x]] = 
+            [p.shape[y][x], p.shape[x][y]];
+    }
+  }
+  
+        // Reverse the order of the columns.
+        p.shape.forEach(row => row.reverse());
+        
+        return clone;
+      }
+
 }
