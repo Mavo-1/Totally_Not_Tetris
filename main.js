861fba3856819f2d8e873674ae8e5e336e61a16d
Alejandro Garcia <agarciacoding@gmail.com>
2022-12-22 13:51:40 -0600

    main update

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d')
 
-ctx.canvas.width = COLS * BLOCK_SIZE;
-ctx.canvas.height = ROWS * BLOCK_SIZE;
 
-//scale gives size of block as 1 to simplify code
-ctx.scale(BLOCK_SIZE, BLOCK_SIZE)
 
 let board = new Board();
 
